3422424215ef02b810008a2c11b7d44a22fd546f
jaet65 <agrazmario@outlook.com>
2025-12-28 18:34:36 -0600

    app version configuration

--- update-version.js
+++ update-version.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
-// Generar una versión basada en la fecha y hora (Ej: 2023.12.18.1530)
+// Generar una versión basada en la fecha y hora (Ej: 2023.12.18@15:30)
 const now = new Date();
-const version = `${now.getFullYear()}.${now.getMonth() + 1}.${now.getDate()}.${now.getHours()}${now.getMinutes()}`;
+const version = `${now.getFullYear()}.${now.getMonth() + 1}.${now.getDate()}@${now.getHours()}:${now.getMinutes()}`;
 
 const indexPath = path.join(__dirname, 'public', 'index.html');
 let content = fs.readFileSync(indexPath, 'utf8');
