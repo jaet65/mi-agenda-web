a151bd5147d214d53134825f2a61f328adfd779c
jaet65 <agrazmario@outlook.com>
2026-01-07 13:21:47 -0600

    Eliminar Mes

--- update-version.js
+++ update-version.js
@@ -36,7 +36,7 @@ if (!indexPath) {
 
 // 3. Generar datos
 const fechaActual = obtenerFechaCDMX();
-const version = `${Math.floor(Date.now() / 100000000)}`;
+const version = `2026`;
 
 console.log(`ℹ️ Archivo encontrado en: ${indexPath}`);
 console.log(`ℹ️ Generando versión: ${version} | Fecha: ${fechaActual}`);
