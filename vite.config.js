43b0ea83b3baabf21f43cde9a28f0dd02f8d19ce
jaet65 <agrazmario@outlook.com>
2026-09-24 19:39:47 -0600

    Calculadora en movil y ancho de franja

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default defineConfig({
   server: {
     port: 5173,
     host: true,
+    https: false, // Habilita HTTPS
   },
+  plugins: [
+    basicSsl() // Genera automáticamente un certificado SSL válido para desarrollo local
+  ],
   publicDir: "public",
   build: {
     outDir: "dist",
